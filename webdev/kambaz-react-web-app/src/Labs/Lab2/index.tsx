abf6e482627f67be3368dc3b4b06b8e894e78b2a
Dina Barua <barua.d@northeastern.edu>
2025-04-19 18:20:40 -0400

    Chapter 2: Added ID-based CSS styling in Lab2

--- webdev/kambaz-react-web-app/src/Labs/Lab2/index.tsx
+++ webdev/kambaz-react-web-app/src/Labs/Lab2/index.tsx
@@ -6,14 +6,12 @@ export default function Lab2() {
       <h2>Lab 2 - Cascading Style Sheets</h2>
       <h3>Styling with the STYLE attribute</h3>
 
+      {/* 2.1.2 & 2.1.3: Paragraphs with IDs instead of inline style */}
       <div id="wd-css-id-selectors">
         <h3>ID selectors</h3>
-
         <p id="wd-id-selector-1">
-          Instead of changing the look and feel of all the elements of the same name, e.g., <i>p</i>,
-          we can refer to a specific element by its ID
+          Instead of changing the look and feel of all the elements of the same name, e.g., p, we can refer to a specific element by its ID
         </p>
-
         <p id="wd-id-selector-2">
           Here's another paragraph using a different ID and a different look and feel
         </p>
