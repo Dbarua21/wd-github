dab7dc697fd0327a7b31d13ccd4fb7356a3ba6dd
Dina Barua <barua.d@northeastern.edu>
2025-04-20 18:53:05 -0400

    Added Bootstrap form and dropdown examples

---
+++ webdev/kambaz-react-web-app/src/Labs/Lab2/BootstrapForms.tsx
@@ -0,0 +1,28 @@
+import { FormControl, FormGroup, FormLabel, FormSelect } from "react-bootstrap";
+
+export default function BootstrapForms() {
+  return (
+    <div id="wd-css-styling-forms">
+      <h2>Forms</h2>
+      <FormGroup className="mb-3" controlId="wd-email">
+        <FormLabel>Email address</FormLabel>
+        <FormControl type="email" placeholder="name@example.com" />
+      </FormGroup>
+
+      <FormGroup className="mb-3" controlId="wd-textarea">
+        <FormLabel>Example textarea</FormLabel>
+        <FormControl as="textarea" rows={3} />
+      </FormGroup>
+
+      <div id="wd-css-styling-dropdowns">
+        <h3>Dropdowns</h3>
+        <FormSelect>
+          <option selected>Open this select menu</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </FormSelect>
+      </div>
+    </div>
+  );
+}
