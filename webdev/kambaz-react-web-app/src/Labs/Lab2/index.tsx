265492c8015ae2d5101cab8a5c4a22a689bc6097
Dina Barua <barua.d@northeastern.edu>
2025-04-20 15:49:07 -0400

    Add React icons

--- webdev/kambaz-react-web-app/src/Labs/Lab2/index.tsx
+++ webdev/kambaz-react-web-app/src/Labs/Lab2/index.tsx
@@ -6,11 +6,13 @@ import Flex from "./Flex";
 import Float from "./Float";
 import ForegroundColors from "./ForegroundColors";
 import GridLayout from "./GridLayout";
-import "./index.css";
 import Margins from "./Margins";
 import Padding from "./Padding";
 import Positions from "./Positions";
-import ZIndex from "./ZIndex";
+import ReactIconsSampler from "./ReactIcons";
+import Zindex from "./Zindex";
+
+import "./index.css";
 
 export default function Lab2() {
   return (
@@ -18,7 +20,7 @@ export default function Lab2() {
       <h2>Lab 2 – Cascading Style Sheets</h2>
 
       <h3>Styling with the STYLE attribute</h3>
-      <p>
+      <p style={{ backgroundColor: "green", color: "white" }}>
         Style attribute allows configuring look and feel right on the element.
         Although it's very convenient it is considered bad practice and you
         should avoid using the style attribute
@@ -53,15 +55,13 @@ export default function Lab2() {
           <h3>Document structure selectors</h3>
           <div className="wd-selector-2">
             <p className="wd-selector-3">
-              This paragraph's red background is referenced as
-              <br />
-              .selector-2 .selector-3
-              <br />
-              meaning the descendant of some ancestor.
+              This paragraph's red background is referenced as <br />
+              .selector-2 .selector-3<br />
+              meaning the descendant of some ancestor.<br />
+              <span className="wd-selector-4">
+                whereas this span is a direct child of its parent
+              </span>
             </p>
-            <span className="wd-selector-4">
-              whereas this span is a direct child of its parent
-            </span>
           </div>
         </div>
       </div>
@@ -69,15 +69,16 @@ export default function Lab2() {
       <ForegroundColors />
       <BackgroundColors />
       <Borders />
-      <Corners />
       <Padding />
       <Margins />
+      <Corners />
       <Dimensions />
       <Positions />
-      <ZIndex />
+      <Zindex />
       <Float />
       <GridLayout />
       <Flex />
+      <ReactIconsSampler />
     </div>
   );
 }
