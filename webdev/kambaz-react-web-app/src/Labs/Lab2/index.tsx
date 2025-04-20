4483801594b72a37b09a67b4ebee1734c2a386d7
Dina Barua <barua.d@northeastern.edu>
2025-04-20 14:01:38 -0400

    Added grid and flex layout components

--- webdev/kambaz-react-web-app/src/Labs/Lab2/index.tsx
+++ webdev/kambaz-react-web-app/src/Labs/Lab2/index.tsx
@@ -2,14 +2,15 @@ import BackgroundColors from "./BackgroundColors";
 import Borders from "./Borders";
 import Corners from "./Corners";
 import Dimensions from "./Dimensions";
+import Flex from "./Flex";
 import Float from "./Float";
 import ForegroundColors from "./ForegroundColors";
+import GridLayout from "./GridLayout";
+import "./index.css";
 import Margins from "./Margins";
 import Padding from "./Padding";
 import Positions from "./Positions";
 import ZIndex from "./ZIndex";
-
-import "./index.css";
 
 export default function Lab2() {
   return (
@@ -17,37 +18,46 @@ export default function Lab2() {
       <h2>Lab 2 – Cascading Style Sheets</h2>
 
       <h3>Styling with the STYLE attribute</h3>
-      <p style={{ backgroundColor: "green", color: "white" }}>
+      <p>
         Style attribute allows configuring look and feel right on the element.
-        Although it's very convenient it is considered bad practice and you should avoid using the style attribute
+        Although it's very convenient it is considered bad practice and you
+        should avoid using the style attribute
       </p>
 
       <h3>ID selectors</h3>
       <div id="wd-css-id-selectors">
         <p id="wd-id-selector-1">
-          Instead of changing the look and feel of all the elements of the same name, e.g., <i>p</i>, we can refer to a specific element by its ID
+          Instead of changing the look and feel of all the elements of the same
+          name, e.g., <i>p</i>, we can refer to a specific element by its ID
         </p>
         <p id="wd-id-selector-2">
-          Here's another paragraph using a different ID and a different look and feel
+          Here's another paragraph using a different ID and a different look and
+          feel
         </p>
       </div>
 
+      <h3>Class selectors</h3>
       <div id="wd-css-class-selectors">
-        <h3>Class selectors</h3>
         <p className="wd-class-selector">
-          Instead of using IDs to refer to elements, you can use an element's CLASS attribute
+          Instead of using IDs to refer to elements, you can use an element's
+          CLASS attribute
         </p>
         <h4 className="wd-class-selector">
           This heading has same style as paragraph above
         </h4>
       </div>
 
+      <h3>Document structure selectors</h3>
       <div id="wd-css-document-structure">
-        <h3>Document structure selectors</h3>
         <div className="wd-selector-1">
+          <h3>Document structure selectors</h3>
           <div className="wd-selector-2">
             <p className="wd-selector-3">
-              This paragraph's red background is referenced as <br /> .selector-2 .selector-3<br /> meaning the descendant of some ancestor.
+              This paragraph's red background is referenced as
+              <br />
+              .selector-2 .selector-3
+              <br />
+              meaning the descendant of some ancestor.
             </p>
             <span className="wd-selector-4">
               whereas this span is a direct child of its parent
@@ -59,13 +69,15 @@ export default function Lab2() {
       <ForegroundColors />
       <BackgroundColors />
       <Borders />
+      <Corners />
       <Padding />
       <Margins />
-      <Corners />
       <Dimensions />
       <Positions />
       <ZIndex />
       <Float />
+      <GridLayout />
+      <Flex />
     </div>
   );
 }
