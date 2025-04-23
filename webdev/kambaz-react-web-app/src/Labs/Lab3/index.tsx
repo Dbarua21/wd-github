c6f37d038db692d7183e285ac35bd2def3e5d1b6
Dina Barua <barua.d@northeastern.edu>
2025-04-23 12:27:36 -0400

    Added conditional files for lab 3

--- webdev/kambaz-react-web-app/src/Labs/Lab3/index.tsx
+++ webdev/kambaz-react-web-app/src/Labs/Lab3/index.tsx
@@ -1,8 +1,23 @@
+import BooleanVariables from "./BooleanVariables.tsx";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse.tsx";
+import ConditionalOutputInline from "./ConditionalOutputInline.tsx";
+import IfElse from "./IfElse.tsx";
+import TernaryOperator from "./TernaryOperator.tsx";
+import VariablesAndConstants from "./VariablesAndConstants.tsx";
+import VariableTypes from "./VariableTypes.tsx";
+
 export default function Lab3() {
-    return (
-      <div>
-        <h2>Lab 3</h2>
-      </div>
-    );
-  }
-  
+  return (
+    <div id="wd-lab3">
+      <h3>Lab 3</h3>
+
+      <VariablesAndConstants />
+      <VariableTypes />
+      <BooleanVariables />
+      <IfElse />
+      <TernaryOperator />
+      <ConditionalOutputIfElse />
+      <ConditionalOutputInline />
+    </div>
+  );
+}
