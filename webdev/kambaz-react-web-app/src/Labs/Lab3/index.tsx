1b3e4820eeaed2ae078dc7d02821d34403510d8e
Dina Barua <barua.d@northeastern.edu>
2025-04-23 22:03:12 -0400

    added in files

--- webdev/kambaz-react-web-app/src/Labs/Lab3/index.tsx
+++ webdev/kambaz-react-web-app/src/Labs/Lab3/index.tsx
@@ -1,11 +1,16 @@
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
 import ArrayIndexAndLength from "./ArrayIndexAndLength";
 import ArrowFunctions from "./ArrowFunctions";
 import BooleanVariables from "./BooleanVariables";
 import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
 import ConditionalOutputInline from "./ConditionalOutputInline";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import ForLoops from "./ForLoops";
 import IfElse from "./IfElse";
 import ImpliedReturn from "./ImpliedReturn";
 import LegacyFunctions from "./LegacyFunctions";
+import MapFunction from "./MapFunction";
 import SimpleArrays from "./SimpleArrays";
 import TemplateLiterals from "./TemplateLiterals";
 import TernaryOperator from "./TernaryOperator";
@@ -16,7 +21,6 @@ export default function Lab3() {
   return (
     <div id="wd-lab3">
       <h3>Lab 3</h3>
-
       <VariablesAndConstants />
       <VariableTypes />
       <BooleanVariables />
@@ -30,6 +34,11 @@ export default function Lab3() {
       <TemplateLiterals />
       <SimpleArrays />
       <ArrayIndexAndLength />
+      <AddingAndRemovingToFromArrays />
+      <ForLoops />
+      <MapFunction />
+      <FindFunction />
+      <FindIndex />
     </div>
   );
 }
